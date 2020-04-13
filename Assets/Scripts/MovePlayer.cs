447854d51d42abc1e13e47ad1ceaa7409255fc7b
Leonie Koch <lk.leonie.koch@gmail.com>
2020-04-14 00:11:59 +0200

    Abgabe 01 - Rotation

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -13,11 +13,23 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        float z = Input.GetAxis("Vertical")  * Time.deltaTime * 3.0f;
+        // float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        // float z = Input.GetAxis("Vertical")  * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0,x,0);
-        transform.Translate(x,0,z);
+        // transform.Rotate(0,x,0);
+        // transform.Translate(x,0,z);
+        
+        // //tbd
+        // Quaternion rotation = Quaternion.LookRotation(x, Vector3.up);
+        // transform.rotation = rotation;
+
+        float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+        float moveVertical = Input.GetAxisRaw ("Vertical");
+  
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.rotation = Quaternion.LookRotation(movement);
+  
+        transform.Translate (movement * 3.0f * Time.deltaTime, Space.World);
         
     }
 }
