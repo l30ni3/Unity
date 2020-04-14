226fbc3a1543e35efb45acc6d6cc7827bee70f9f
Leonie Koch <lk.leonie.koch@gmail.com>
2020-04-14 09:43:10 +0200

    Abgabe 01 - Kamera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //Public variable to store a reference to the player game object
-    public GameObject player;        
+    // Variable to store a reference to the player game object
+    private Transform target;
 
     //Private variable to store the offset distance between the player and camera
-    private Vector3 offset;
-    
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 offsetPosition;
+    private Space offsetPositionSpace = Space.Self;
+
+    private bool lookAt = true;
+
+    private void LateUpdate()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        Refresh();
     }
 
-    // LateUpdate is called after Update each frame
-    void LateUpdate () 
+    public void Refresh()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
     }
 }
--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    public float moveSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        // float z = Input.GetAxis("Vertical")  * Time.deltaTime * 3.0f;
-
-        // transform.Rotate(0,x,0);
-        // transform.Translate(x,0,z);
-        
-        // //tbd
-        // Quaternion rotation = Quaternion.LookRotation(x, Vector3.up);
-        // transform.rotation = rotation;
-
         float moveHorizontal = Input.GetAxisRaw ("Horizontal");
         float moveVertical = Input.GetAxisRaw ("Vertical");
   
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveSpeed * moveHorizontal* Time.deltaTime, 0.0f, moveSpeed * moveVertical * Time.deltaTime);
         transform.rotation = Quaternion.LookRotation(movement);
   
-        transform.Translate (movement * 3.0f * Time.deltaTime, Space.World);
-        
+        transform.Translate (movement, Space.World);
     }
 }
