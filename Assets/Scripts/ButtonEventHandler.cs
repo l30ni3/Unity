db95548b68c0d10cf86614b862091a2fd94c1a46
Leonie Koch <lk.leonie.koch@gmail.com>
2020-05-03 21:06:48 +0200

    Added Restart Button Behavior

--- Assets/Scripts/ButtonEventHandler.cs
+++ Assets/Scripts/ButtonEventHandler.cs
@@ -5,39 +5,39 @@ using Vuforia;
 
 public class ButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler{
 
-public Transform Player;
+    public Transform Player;
 
-void Start() {
-    // Search for all Children from this ImageTarget with type VirtualButtonBehaviour
-    VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
-    for (int i = 0; i < vbs.Length; ++i){
-        // Register with the virtual buttons TrackableBehaviour
-        vbs[i].RegisterEventHandler(this);
+    void Start() {
+        // Search for all Children from this ImageTarget with type VirtualButtonBehaviour
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i){
+            // Register with the virtual buttons TrackableBehaviour
+            vbs[i].RegisterEventHandler(this);
+        }
     }
-}
 
-public void OnButtonPressed(VirtualButtonBehaviour vb){ 
-    switch (vb.VirtualButtonName){
-        case "Up":
-            Debug.Log("Button Up");
-            Player.Transform.Translate(Vector3.forward * 15.0f * Time.deltaTime, Space.World);
-        break;
-        case "Down":
-            Debug.Log("Button Down");
-            Player.Transform.Translate(-Vector3.forward * 15.0f * Time.deltaTime, Space.World);
-        break;
-        case "Left":
-            Debug.Log("Button Left");
-            Player.transform.position += Vector3.left * 15.0f * Time.deltaTime;
-            // Player.transform.Rotate (new Vector3(0, -(Time.deltaTime * 1000), 0));
-        break;
-        case "Right":
-            Debug.Log("Button Right");
-            Player.transform.position += Vector3.right * 15.0f * Time.deltaTime;
-            // Player.transform.Rotate (new Vector3(0, Time.deltaTime * 1000, 0));
-        break;
+    public void OnButtonPressed(VirtualButtonBehaviour vb){ 
+        switch (vb.VirtualButtonName){
+            case "Up":
+                Debug.Log("Button Up");
+                Player.transform.Translate(Vector3.forward * 15.0f * Time.deltaTime, Space.World);
+            break;
+            case "Down":
+                Debug.Log("Button Down");
+                Player.transform.Translate(-Vector3.forward * 15.0f * Time.deltaTime, Space.World);
+            break;
+            case "Left":
+                Debug.Log("Button Left");
+                Player.transform.position += Vector3.left * 15.0f * Time.deltaTime;
+                // Player.transform.Rotate (new Vector3(0, -(Time.deltaTime * 1000), 0));
+            break;
+            case "Right":
+                Debug.Log("Button Right");
+                Player.transform.position += Vector3.right * 15.0f * Time.deltaTime;
+                // Player.transform.Rotate (new Vector3(0, Time.deltaTime * 1000, 0));
+            break;
+        }
     }
-}
 
-public void OnButtonReleased(VirtualButtonBehaviour vb) { }
+    public void OnButtonReleased(VirtualButtonBehaviour vb) { }
 }
