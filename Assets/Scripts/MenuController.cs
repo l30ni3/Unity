403563b2b0b56f54864f67cfb593eb5cd8ba6a21
Leonie Koch <lk.leonie.koch@gmail.com>
2020-05-03 19:28:34 +0200

    Created Menu Controller Script

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject Menu;
+    bool gamePaused;
+
+    void Update(){
+        //Reading input for ESCAPE key, and by saying gamePaused = !gamePaused, we switch the bool on and off each time the Keycode is registered!
+        if(Input.GetKeyDown(KeyCode.Escape))
+            gamePaused = !gamePaused;
+ 
+        //Now we enable and disable the game object!
+        if(gamePaused)
+            Time.timeScale = 0;
+            canvasObj.SetActive(true);
+        else
+            canvasObj.SetActive(false);
+ 
+    }
+}
